aa052885577e6d2db8b4fcc72fc527ea0786c2d0
JordanSerafini <jordan@solution-logique.fr>
2024-02-26 16:33:05 +0100

    :construction: working on planing

--- src/services/EventOfTheDay.tsx
+++ src/services/EventOfTheDay.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import dataContext, { DataContextType } from "../context/dataContext";
 
 import "react-calendar/dist/Calendar.css";
