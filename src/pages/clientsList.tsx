df0083cdc296ab469776a1c80e144b8581b14348
JordanSerafini <jordan@solution-logique.fr>
2024-02-26 14:47:30 +0100

    :construction: add debounce, working on client List and detail, using daisyUI for tailwind

--- src/pages/clientsList.tsx
+++ src/pages/clientsList.tsx
@@ -1,9 +1,15 @@
 
+import CircleLoader from "../components/loader/circleLoader";
+import { IsDataFetched } from "../hooks/isDataFetched"; 
+
 
 function clientsList() {
+  const isLoading = IsDataFetched();
 
 
-  
+  if (isLoading) {
+    return <CircleLoader />; 
+  }
   return (
     <div>clientsList</div>
   )
--- src/pages/planing.tsx
+++ src/pages/planing.tsx
@@ -1,5 +1,12 @@
+import CircleLoader from "../components/loader/circleLoader";
+import { IsDataFetched } from "../hooks/isDataFetched"; 
 
 function planing() {
+  const isLoading = IsDataFetched();
+  
+  if (isLoading) {
+    return <CircleLoader />; 
+  }
   return (
     <div>planing</div>
   )
