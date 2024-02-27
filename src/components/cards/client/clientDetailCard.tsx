e6e3d21c85a886ddab73cfa88c5c7d048a759523
JordanSerafini <jordan@solution-logique.fr>
2024-02-27 16:10:30 +0100

    :construction: add stock toast and css generally

--- src/components/cards/client/clientDetailCard.tsx
+++ src/components/cards/client/clientDetailCard.tsx
@@ -25,33 +25,43 @@ const clientDetailCard: React.FC<DetailClientProps> = ({ selectedClient }) => {
 
       <div className="flex flex-row gap-4 justify-evenly">
         <div className="flex gap-2">
-          <a href={`tel:${selectedClient.maindeliverycontact_cellphone}`}>
-            <img src={telLogo} alt="Téléphone" className="h-4" />
-          </a>
-          <a
-            href={`tel:${selectedClient.maindeliverycontact_cellphone}`}
-            className="no-underline text-black"
-          >
-            <div>{selectedClient.maindeliverycontact_cellphone}</div>
-          </a>
+          {selectedClient.maindeliverycontact_cellphone && (
+            <>
+              <a href={`tel:${selectedClient.maindeliverycontact_cellphone}`}>
+                <img src={telLogo} alt="Téléphone" className="h-4" />
+              </a>
+              <a
+                href={`tel:${selectedClient.maindeliverycontact_cellphone}`}
+                className="no-underline text-black"
+              >
+                <div>{selectedClient.maindeliverycontact_cellphone}</div>
+              </a>
+            </>
+          )}
         </div>
         <div className="flex gap-2">
-          <a href={`tel:${selectedClient.maininvoicingcontact_phone}`}>
-            <img src={telLogo} alt="Téléphone" className="h-4" />
-          </a>
-          <a
-            href={`tel:${selectedClient.maininvoicingcontact_phone}`}
-            className="no-underline text-black"
-          >
-            <div>{selectedClient.maininvoicingcontact_phone}</div>
-          </a>
+          {selectedClient.maininvoicingcontact_phone && (
+            <>
+              <a href={`tel:${selectedClient.maininvoicingcontact_phone}`}>
+                <img src={telLogo} alt="Téléphone" className="h-4" />
+              </a>
+              <a
+                href={`tel:${selectedClient.maininvoicingcontact_phone}`}
+                className="no-underline text-black"
+              >
+                <div>{selectedClient.maininvoicingcontact_phone}</div>
+              </a>
+            </>
+          )}
         </div>
         <div>
-          <a
-            href={`mailto:${selectedClient.maininvoicingcontact_email}?subject=Sujet de l'email&body=Corps de l'email`}
-          >
-            <img src={mailLogo} alt="Email" className="h-8" />
-          </a>
+          {selectedClient.maininvoicingcontact_email && (
+            <a
+              href={`mailto:${selectedClient.maininvoicingcontact_email}?subject=Sujet de l'email&body=Corps de l'email`}
+            >
+              <img src={mailLogo} alt="Email" className="h-8" />
+            </a>
+          )}
         </div>
       </div>
 
