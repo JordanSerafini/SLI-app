313bfa62dc1035029b46361299ce0a1f9857aea0
JordanSerafini <jordan@solution-logique.fr>
2024-03-06 17:24:26 +0100

    work on form

--- src/components/form/partieContainer.tsx
+++ src/components/form/partieContainer.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Question from "./question";
 import Rating from "./rating";
 
-
 const PartieContainer: React.FC = () => {
   const [ratingValue, setRatingValue] = useState(0);
 
@@ -12,16 +11,28 @@ const PartieContainer: React.FC = () => {
   };
 
   return (
-    // 1ere Partie --- nom - numero - date
-    // 2eme Partie 
-    <div className="w-9.5/10 bg-white rounded-xl flex flex-col gap-6" >
-      < Question title="Quel est votre avis concernatn le suivi de validation?" numero="1" partieID={1} />
-      < Question title="Seconde question du questionnaire de satifsaction ?" numero="1" partieID={1} />
-      < Rating title="Quel est votre avis concernatn le suivi de validation?" onChange={handleRatingChange}/>
-      <p>valeur: {ratingValue}</p>
+    <div>
+      {/*--------------------------- 1ere Partie --- nom - numero - date  ------------------------*/}
+      {/*--------------------------- 2eme Partie ----------------------------------------*/}
+      <div className="w-9.5/10 bg-white rounded-xl flex flex-col gap-6 p-2 h-fit">
+        <Question
+          title="Quel est votre avis concernatn le suivi de validation?"
+          numero="1"
+          partieID={1}
+        />
+        <Question
+          title="Seconde question du questionnaire de satifsaction ?"
+          numero="1"
+          partieID={1}
+        />
+        <Rating
+          title="Quel est votre avis concernatn le suivi de validation?"
+          onChange={handleRatingChange}
+        />
+        <p>valeur: {ratingValue}</p>
+      </div>
     </div>
-    
   );
-}
+};
 
 export default PartieContainer;
