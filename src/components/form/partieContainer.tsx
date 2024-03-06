235f961f0508d316205e87e4899fe7fcbc84ddbd
JordanSerafini <jordan@solution-logique.fr>
2024-03-06 13:57:28 +0100

    working on booking, scrap first page title and describe ok

--- src/components/form/partieContainer.tsx
+++ src/components/form/partieContainer.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Question from "./question";
 import Rating from "./rating";
 
 
 const PartieContainer: React.FC = () => {
-  
+  const [ratingValue, setRatingValue] = useState(0);
+
+  const handleRatingChange = (value: number) => {
+    setRatingValue(value); // Mettre à jour la valeur d'évaluation dans l'état
+    // Vous pouvez également effectuer d'autres actions ici, comme envoyer la valeur à un serveur
+  };
 
   return (
     // 1ere Partie --- nom - numero - date
@@ -11,7 +17,8 @@ const PartieContainer: React.FC = () => {
     <div className="w-9.5/10 bg-white rounded-xl flex flex-col gap-6">
       < Question title="Quel est votre avis concernatn le suivi de validation?" numero="1" partieID={1} />
       < Question title="Seconde question du questionnaire de satifsaction ?" numero="1" partieID={1} />
-      < Rating />
+      < Rating title="test" onChange={handleRatingChange}/>
+      <p>valeur: {ratingValue}</p>
     </div>
     
   );
--- src/components/form/rating.tsx
+++ src/components/form/rating.tsx
@@ -1,13 +1,26 @@
+import {  useState } from 'react';
 
-function rating() {
+function Rating({ title, onChange }) {
+
+  const handleRatingChange = (value:number) => {
+    setSelectedRating(value);
+  };
+
+
+
+
   return (
-<div className="rating rating-lg">
-  <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-</div>  )
+    <div>
+        <h3>{title}</h3>
+    <div className="rating rating-lg">
+      <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" onClick={() => handleRatingChange(1)} />
+      <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" onClick={() => handleRatingChange(2)} />
+      <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" onClick={() => handleRatingChange(3)} />
+      <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" onClick={() => handleRatingChange(4)} />
+      <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" onClick={() => handleRatingChange(5)} />
+    </div>
+    </div>
+  );
 }
 
-export default rating
+export default Rating;
