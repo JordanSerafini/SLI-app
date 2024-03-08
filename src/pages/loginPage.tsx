2e05b757e0ab3fa17b6aca4da60cf629949cee88
JordanSerafini <jordan@solution-logique.fr>
2024-03-08 12:34:45 +0100

    work on form

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -41,13 +41,13 @@ const LoginPage = () => {
 
   return (
     <div className="bg-bg-2 h-screen w-screen flex flex-col items-center justify-center">
-      <div className="h-9.5/10 w-9/10 bg-white rounded-lg p-2 flex flex-col justify-evenly">
+      <div className="h-9.5/10 w-9/10 bg-white rounded-lg p-2 flex flex-col justify-evenly items-center">
         <div>
           <img src={sliLogo} alt="" />
         </div>
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col items-center gap-2"
+          className="flex flex-col items-center gap-2 w-full"
         >
           <label htmlFor="email" className="">
             Email:
@@ -58,7 +58,7 @@ const LoginPage = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className="border-1 border-blue-2 p-2 rounded-3xl w-9/10 focus:border-blue-2 custom-input"
+            className="border-1 border-blue-2 p-2 rounded-3xl w-9/10 focus:border-blue-2 custom-input md:w-6/10 lg:w-5/10"
           />
           <label htmlFor="password">Password:</label>
           <input
@@ -67,7 +67,7 @@ const LoginPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            className="border-1 border-blue-2 p-2 rounded-3xl w-9/10 focus:border-blue-2 custom-input"
+            className="border-1 border-blue-2 p-2 rounded-3xl w-9/10 focus:border-blue-2 custom-input md:w-6/10 lg:w-5/10"
           />
           <button type="submit">Login</button>
         </form>
