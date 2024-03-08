90bc1aa387097d92b5b4b406a4fe388df20a05d4
JordanSerafini <jordan@solution-logique.fr>
2024-03-08 13:45:17 +0100

    work on form

--- src/pages/formPage.tsx
+++ src/pages/formPage.tsx
@@ -6,7 +6,7 @@ function FormPage() {
 
 
   return (
-    <div className="bg-bg-lightgray mb-20 h-screen w-screen flex flex-col items-center overflow-auto">
+    <div className="bg-bg-lightgray h-screen w-screen flex flex-col items-center overflow-auto">
   
         <PartieContainer/>
 
