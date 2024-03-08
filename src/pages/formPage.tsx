9e80dd568b51797cba7ab6a218d201cb08352a2b
JordanSerafini <jordan@solution-logique.fr>
2024-03-08 10:20:10 +0100

    work on form

--- src/pages/formPage.tsx
+++ src/pages/formPage.tsx
@@ -1,65 +1,50 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PartieContainer from "../components/form/partieContainer";
 import url from "../axios/url";
 
 function FormPage() {
   const [isValidToken, setIsValidToken] = useState<boolean | null>(null);
-  const [tokenData, setTokenData] = useState({});
+  const [tokenDta, setTokenData] = useState<{
+    email: string;
+    exp: number;
+    iat: number;
+  } | null>(null);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
-    console.log(token);
-    const validateTokenUrl = `${url.main}/verifyToken?token=${token}`;
 
     if (token) {
-        console.log("Envoi de la requête de validation :", validateTokenUrl);
+      const validateTokenUrl = `${url.main}/verifyToken?token=${token}`;
 
-        fetch(validateTokenUrl)
-            .then((response) => {
-                console.log('Statut de la réponse du serveur :', response.status);
-
-                if (!response.ok) {
-                    throw new Error("Réponse serveur invalide");
-                }
-
-                return response.json();
-            })
-            .then((data) => {
-                console.log("Réponse serveur :", data);
-
-                if (data.isValid) {
-                    console.log("Token valide :", data.data);
-                    setIsValidToken(true);
-                    setTokenData(data.data);
-                } else {
-                    console.error("Token invalide :", data.error);
-                    setIsValidToken(false);
-                }
-            })
-            .catch((error) => {
-                console.error("Erreur lors de la validation du token :", error);
-                setIsValidToken(false);
-            });
+      fetch(validateTokenUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Réponse serveur invalide");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setIsValidToken(true);
+          setTokenData(data.decoded);
+          console.log("Token valide :", data.decoded);
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la validation du token :", error);
+          setIsValidToken(false);
+        });
     } else {
-        console.error("Token manquant dans l'URL");
-        setIsValidToken(false);
+      console.error("Token manquant dans l'URL");
+      setIsValidToken(false);
     }
-}, []);
-
-  useEffect(() => {
-    if (isValidToken) {
-      console.log("Token valide:", tokenData);
-    }
-  }, [isValidToken, tokenData]);
-
+  }, []);
 
   return (
     <div className="bg-bg-lightgray mb-20 h-screen w-screen flex flex-col items-center overflow-auto">
       {isValidToken === null ? (
         <p>Vérification du token en cours...</p>
       ) : isValidToken ? (
-        <PartieContainer />
+        <PartieContainer/>
       ) : (
         <p>
           Token invalide ou expiré. Veuillez vérifier votre lien ou contacter
